8526fe4a2d06094710432436817be5eca4fb9f9c
Timothy Goncharov <sedmoy.mm@mail.ru>
2017-10-25 16:57:27 +0300

    Created abstract styles changed config.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import createStore from './store/create-store';
+import createStore from './redux-store/create-store';
 import App from './containers/app';
 
 const store = createStore();
